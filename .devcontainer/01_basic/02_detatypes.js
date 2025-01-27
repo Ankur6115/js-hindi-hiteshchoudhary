688d3be986dda441ee414dd7f06b303ae76f8eee
aankurr <Ankur724070@gmail.com>
2025-01-27 18:50:42 +0000

    deta type

---
+++ .devcontainer/01_basic/02_detatypes.js
@@ -0,0 +1,27 @@
+"use strict"; // traet all js code as new version
+
+/*
+alert( 3 + 3 ) we are using nodejs, not browser
+*/
+
+console.log(3
+    +
+    3
+); // code readability should be high
+
+
+let name = "ankur"
+let age = 18
+let isloggedIn = true
+
+// number => 2 to power 53
+// bigint  jab number value bade ho tab use karte hai, ex=> facebook number , stock ,etc
+
+//string => ""
+// boolean => true/false
+// null => standalone value
+// undefind => 
+    // symbol => unique
+
+
+// object
---
+++ 01_varibables.js
@@ -0,0 +1,18 @@
+const accountId = 144553
+let accountEmail = "ankjdei@gmail.accountEmail"
+var accountPassword = "12345"
+accountCity = "jaipur"
+let acoountState;
+
+console.table([accountId, accountEmail, accountPassword, accountCity, acoountState]);
+//value change karte h
+
+accountEmail = "ankur@gmail.com"
+accountPassword = "789456"
+accountCity = "delhi"
+ /*
+ perfer not use var
+ because of issuein block scop and function scop
+
+ */
+console.table([accountId, accountEmail, accountPassword, accountCity]);
