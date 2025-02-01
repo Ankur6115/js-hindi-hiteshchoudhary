5178ad4a088daf756731bc5d8148f14892ea3c7f
aankurr <Ankur724070@gmail.com>
2025-02-01 17:53:56 +0000

    stack and heap

---
+++ detatypes-summary.js
@@ -0,0 +1,40 @@
+// data delita ho geya ya ka 
+
+
+
+
+
+//**********************************stack and  heap ************************************************** */
+
+//stack (primitive),  heap(non-primitive)
+
+
+let myyoutubename = "ankurtechcom"
+let anothername = myyoutubename
+
+anothername = "ankur724070@gmail.com"
+
+console.log(anothername);
+console.log(myyoutubename);
+
+
+//***HEAP */
+
+
+let userone = {
+    email :"user@gmail.com"
+
+
+}
+
+let usertwo = userone
+
+usertwo.email = "ankur7241071@gmail.com"
+
+
+console.log(userone.email);
+console.log(usertwo.email);
+
+
+
+
