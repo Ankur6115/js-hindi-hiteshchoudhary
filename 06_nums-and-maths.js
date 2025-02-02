b1d1c793194bd837e3b3e79131351bbbaa78226b
aankurr <Ankur724070@gmail.com>
2025-02-02 19:18:31 +0000

    number and maths

---
+++ 06_nums-and-maths.js
@@ -0,0 +1,39 @@
+const score = 400
+console.log(score);
+
+
+
+const balance = new Number(100)
+console.log(balance);
+
+console.log(balance.toString().length);
+console.log(balance.toFixed(2));
+
+const othernumber = 123.22682
+console.log(othernumber.toPrecision(4));
+
+const hundreds = 1000000
+console.log(hundreds.toLocaleString('en-IN'));
+
+
+//========================maths================================
+console.log(Math.abs(-4));
+
+console.log(Math.round(4.5));
+
+console.log(Math.max(4, 6, 8, 2, 5));
+
+console.log(Math.random());
+console.log(Math.random()*10 +1);
+
+
+
+
+
+
+
+
+
+
+
+
