255ef5d519ade6099015db29709d6a4ade94e525
aankurr <Ankur724070@gmail.com>
2025-02-03 18:43:08 +0000

    arrays

---
+++ 01_arrays.js
@@ -0,0 +1,24 @@
+const myArr = [0, 1, 2, 3, 4, 5, 6]
+const myHeros = ["shaktiman", "naagraj"]
+
+const myArr2 = new Array(1, 2, 3, 4)
+console.log(myArr[1]);
+
+myArr.push(6)
+myArr.pop()
+
+//myArr.unshift(9)
+//myArr.shift()
+
+
+console.log(myArr.includes(3));
+
+const newArr = myArr.join()
+
+
+console.log(myArr);
+console.log(newArr);
+
+
+
+
