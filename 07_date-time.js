5640a2e067287dd28fdbe1bb5b716d5a3a5e325b
aankurr <Ankur724070@gmail.com>
2025-02-03 12:05:47 +0000

    date and time

---
+++ 07_date-time.js
@@ -0,0 +1,23 @@
+
+let myDate = new Date()
+console.log(myDate.toString());
+console.log(myDate.toDateString());
+console.log(myDate.toISOString());
+console.log(myDate.toJSON());
+console.log(myDate.toLocaleString());
+console.log(typeof myDate);
+
+
+
+let myCreatedDate = new Date(2023, 0, 23, 5, 3)
+console.log(myCreatedDate.toLocaleString());
+
+
+let newCreatedDate = new Date("2023-01-14")
+console.log(newCreatedDate.toLocaleString());
+
+let myTimeStamp = Date.now()
+console.log(myTimeStamp);
+
+//learn 12:00
+
