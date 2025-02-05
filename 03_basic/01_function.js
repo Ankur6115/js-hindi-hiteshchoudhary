00854124196f57e79c8b8e529972647f2c359c18
aankurr <Ankur724070@gmail.com>
2025-02-05 13:12:34 +0000

    function part 2

--- 03_basic/01_function.js
+++ 03_basic/01_function.js
@@ -39,3 +39,41 @@ function loginUserMassage(username){
 
 console.log(loginUserMassage("ankur"));
 
+//********************************************************************************* */
+
+
+function calculateCartPrice(...num1){
+    return num1
+}
+
+console.log(calculateCartPrice(200, 400, 500));
+
+const user = {
+    username : "ankur",
+    price : 199,
+}
+
+function handleObject(anyobject){
+
+    console.log(`username is ${anyobject.username} and price is ${anyobject.price}`);
+    
+}
+//handleObject(user)
+
+handleObject({
+    username : "sam",
+    price : 399
+})
+
+
+
+const myNewArray = [200, 400, 100]
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+
+//console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([200, 400, 500, 1000]));
+
+
+
