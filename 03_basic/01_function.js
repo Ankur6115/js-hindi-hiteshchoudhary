550611ccd399e234545a2b84c5a460dfb45d794b
aankurr <Ankur724070@gmail.com>
2025-02-05 11:53:05 +0000

    function

---
+++ 03_basic/01_function.js
@@ -0,0 +1,41 @@
+function sayMyName(){
+    console.log("A");
+    console.log("N");
+    console.log("K");
+    console.log("U");
+    console.log("R");
+    
+}
+
+//sayMyName()
+
+
+//function addTwoNumbers(number1 , number2){
+  //  console.log(number1 + number2);
+    
+//}
+
+//const result = addTwoNumbers(3 , 4)
+//console.log("Result ", result);
+
+
+
+
+
+
+function addTwoNumbers(number1 , number2){
+    let result = number1 + number2
+    return result
+}
+    
+
+
+const result = addTwoNumbers(3 , 4)
+//console.log("Result: ", result);
+
+function loginUserMassage(username){
+    return`${username} just logged in`
+}
+
+console.log(loginUserMassage("ankur"));
+
