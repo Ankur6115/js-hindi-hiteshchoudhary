620c802a11210e8168d140975be8b39e0368ef2a
aankurr <Ankur724070@gmail.com>
2025-02-06 10:35:10 +0000

    scope

---
+++ 03_basic/02_scope.js
@@ -0,0 +1,15 @@
+let a = 300
+if (true) {
+    let a = 10
+    const b = 20
+    console.log("ineer: ", a);
+    
+}
+
+for (let i = 0; i < array.length; i++) {
+    const element = array[i];
+    
+}
+
+
+console.log(a);
