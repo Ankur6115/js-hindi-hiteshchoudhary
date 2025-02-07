bf2fbe20ae1e8299a2484e1219e8a29d4295868e
aankurr <Ankur724070@gmail.com>
2025-02-07 13:09:16 +0000

    iife

---
+++ 03_basic/04_life.js
@@ -0,0 +1,19 @@
+// Immediately Invoked Function Expressions  (IIFE)
+
+
+/*function chai(){
+    console.log(`DB CONNECTED`);
+    
+}
+
+chai()*/
+
+
+( function chai(){
+    console.log(`DB CONNECTED`);
+    
+})();
+
+
+
+
