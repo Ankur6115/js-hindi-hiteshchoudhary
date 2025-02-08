53962bb3713dfa730534ae400bdf1b16a47fbdc8
aankurr <Ankur724070@gmail.com>
2025-02-08 12:35:44 +0000

    one

--- 04_control_flow/one.js
+++ 04_control_flow/one.js
@@ -20,4 +20,32 @@ if (score > 100) {
 
 const balance = 1000
 
-if (balance > 500) console.log("test");
+if (balance < 500) {
+    console.log("less than 500");
+} else if (balance < 750) {
+    console.log("less than 750");
+    
+} else if (balance < 900) {
+    console.log("less than 900");
+    
+} else {
+    console.log("less than 1200");
+    
+}
+
+
+const userLoggedIn = true
+const debitCard = true
+
+if (userLoggedIn && debitCard) {
+    console.log("allow to by course");
+    
+}
+
+const loggedInFromGoogle = true
+const loggedInFromEmail = true
+
+if (loggedInFromGoogle || loggedInFromEmail) {
+    console.log("user logged in");
+    
+}
